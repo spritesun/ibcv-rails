dbb50823d228840be8e6d36c73be3d6a52971e90
Long Sun <spritesun@gmail.com>
2012-12-06 17:51:44 +1100

    Init ibcv

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ibcv::Application.config.secret_token = '7ebdaed519cd9c0da9fb91a21315a1763c8eef2b11572726c4c4de6eebe28899f199bc622d15abfbb524e679fe3474839654095774ee12dcca5d246fe37fbcfe'
